677d908272f2a6c43ab6c89e5c3691267e5ac0ef
Lauri Tirkkonen <lotheac@iki.fi>
2012-12-31 02:27:01 +0200

    working room exit parsing

--- mapper.py
+++ mapper.py
@@ -1,6 +1,7 @@
 import sys
 import wx
 import threading
+import networkx as nx
 
 try:
     import tf
@@ -55,3 +56,51 @@ class GUIThread(threading.Thread):
     def start(self):
         super(GUIThread, self).start()
         self._startup_lock.acquire()
+
+class Room(object):
+
+    @classmethod
+    def fromstring(cls, area, roomstr):
+        brief, desc, exitstr = roomstr.split('\n')
+        return cls(area, brief=brief, desc=desc, exitstr=exitstr)
+
+    def __init__(self, area, brief=None, desc=None, exitstr=None):
+        self.area = area
+        self.brief = brief
+        self.desc = None
+        self.exits = set()
+        if exitstr:
+            self.add_exits(exitstr)
+
+    def add_exits(self, exitstr):
+        if not (exitstr.startswith('Obvious exits are: ') or
+                exitstr.startswith('Obvious exit is: ')):
+            return
+        exits = exitstr.split(':')[1].rstrip('.').lstrip()
+        exits = exits.replace(' and ', ', ')
+        exits = exits.split(', ')
+        self.exits.update(map(self._normalize_exit, exits))
+
+    @staticmethod
+    def _normalize_exit(s):
+        exitnames = {
+            'n': 'north',
+            'e': 'east',
+            'w': 'west',
+            's': 'south',
+            'ne': 'northeast',
+            'nw': 'northwest',
+            'se': 'southeast',
+            'sw': 'southwest',
+            'u': 'up',
+            'd': 'down',
+        }
+        if s in exitnames:
+            return exitnames[s]
+        return s
+
+class Area(nx.DiGraph):
+
+    def __init__(self, name):
+        super(Area, self).__init__(directed=True)
+        self.name = name
